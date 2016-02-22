bf86843a62c8cb4d668c248a232fe69686430429
gj <gj@itcast.cn>
2016-02-23 00:39:14 +0800

    初始化项目

---
+++ LZ个人主页/LZ个人主页/Controller/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  LZ个人主页
+//
+//  Created by apple on 16/2/23.
+//  Copyright © 2016年 m14a.cn. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ LZ个人主页/LZ个人主页/Others/AppDelegate.h
@@ -0,0 +1,18 @@
+//
+//  AppDelegate.h
+//  LZ个人主页
+//
+//  Created by apple on 16/2/23.
+//  Copyright © 2016年 m14a.cn. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+
+@end
+
